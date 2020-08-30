8b9e4975a3ae7ea50ca889a83a0d4f9310630341
Yerrasingh <pavan.yerrasingh@broadcom.com>
2020-08-30 13:21:07 +0530

    first commmit

---
+++ src/main/java/com/restservices/springbootbuild/SpringbootBuildApplication.java
@@ -0,0 +1,13 @@
+package com.restservices.springbootbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildApplication.class, args);
+	}
+
+}
