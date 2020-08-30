94f80379cab259809456470b60d5e5a8b561e4fb
vyerrasingh <pavan.yerrasingh@broadcom.com>
2020-08-30 13:40:53 +0530

    second commit

--- src/main/java/com/restservices/springbootbuild/SpringbootBuildApplication.java
+++ src/main/java/com/restservices/springbootbuild/SpringbootBuildApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootBuildApplication {
+	
+	
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBuildApplication.class, args);
